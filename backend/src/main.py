b5b6685f71179e1dcb79738d01e05adb5dec65b6
RyanGrieb <ryanrhin@gmail.com>
2024-12-05 14:55:54 -0600

    Remove redis

--- backend/src/main.py
+++ backend/src/main.py
@@ -9,9 +9,6 @@ from quart_session import Session
 server = Quart(__name__)
 server.secret_key = "opnqpwefqewpfqweu32134j32p4n1234d"
 
-# Setup redis
-server.config["SESSION_TYPE"] = "redis"
-server.config["SESSION_URI"] = "redis://redis:6379"
 Session(server)
 
 
